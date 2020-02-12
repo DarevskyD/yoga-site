6a87c5f5f731ce1483ab47c3729eff593bb4373f
DarevskyD <darevsky.dmitry@gmail.com>
2020-02-12 20:24:02 +0300

    configured file

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './yoga/src/js/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'yoga/dist')
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader?optional[]=runtime',
+        options: {
+          presets: [
+            ["@babel/env", {
+              targets: {
+                edge: "17",
+                firefox: "60",
+                chrome: "67",
+                safari: "11.1",
+                ie: "11"
+              }
+            }]
+          ]
+        }
+      }
+    }]
+  }
+};
